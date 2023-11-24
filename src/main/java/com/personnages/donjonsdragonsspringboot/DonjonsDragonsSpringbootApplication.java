b5759e83964f4115d727fa456b8ec2418ed31342
Rayane KANUKA <rayane.kanuka@Digital-Grenoble.local>
2023-11-24 10:21:37 +0100

    first commit

---
+++ src/main/java/com/personnages/donjonsdragonsspringboot/DonjonsDragonsSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.personnages.donjonsdragonsspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonjonsDragonsSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonjonsDragonsSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personnages/donjonsdragonsspringboot/DonjonsDragonsSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personnages.donjonsdragonsspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonjonsDragonsSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
