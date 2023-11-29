8ce138f801eaf0b9972cf7d1cad36026c93aa4e6
rayanekanuka <r.kanuka-benmahmoud@le-campus-grenoble.fr>
2023-11-29 16:16:57 +0100

    Ajout exceptions en cours

--- src/main/java/com/personnages/donjonsdragonsspringboot/model/Personnage.java
+++ src/main/java/com/personnages/donjonsdragonsspringboot/model/Personnage.java
@@ -2,21 +2,41 @@ package com.personnages.donjonsdragonsspringboot.model;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
-
+import jakarta.validation.constraints.*;
 
 @Entity
+@Table(name = "personnage")
 public class Personnage {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Schema (name = "id", example = "1", required = true)
+    @Column (name = "id", nullable = false)
+    @NotNull
     private int id;
     @Schema (name = "nom", example = "Massimo")
+    @Column (name = "nom", nullable = false)
+    @NotBlank (message = "Le nom est obligatoire")
+    @Size(min = 3, max = 40, message = "Le nom doit être compris entre 3 et 40 caractères")
     private String nom;
+
     @Schema (name = "type", example = "Guerrier")
+    @Column (name = "type", nullable = false)
+    @NotBlank (message = "Le nom type obligatoire")
+    @Size(min = 3, max = 40, message = "Le type doit être compris entre 3 et 40 caractères")
     private String type;
     @Schema (name = "life", example = "15")
     @Column (name = "life", nullable = false)
+    @NotNull
+    @Min(value = 3, message = "Les points de vies ne peuvent pas être en-dessous de 3")
+    @Max(value = 999, message = "Les points de vies ne peuvent pas être au-dessus de 999")
+    @PositiveOrZero(message = "Attention action non autorisée")
     private int vie;
+
+    // ----------------------------- CONSTRUCTEUR ------------------------------- //
+
+    public Personnage() {
+
+    }
 
     public Personnage(int id, String nom, String type, int vie) {
         this.id = id;
@@ -25,9 +45,8 @@ public class Personnage {
         this.vie = vie;
     }
 
-    public Personnage() {
+    // ----------------------------- GETTER & SETTER ------------------------------- //
 
-    }
 
     public int getId() {
         return id;
--- src/main/java/com/personnages/donjonsdragonsspringboot/web/controller/PersonnagesController.java
+++ src/main/java/com/personnages/donjonsdragonsspringboot/web/controller/PersonnagesController.java
@@ -7,9 +7,15 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * RestController indique que cette classe va pouvoir traiter les requêtes que nous allons définir.
@@ -69,12 +75,12 @@ public class PersonnagesController {
     /**
      * Modifie un personnage identifié par un id
      *
-     * @param id
+     * @param
      * @return
      */
     @Operation(summary = "Modifie un personnage selon son id")
     @PutMapping("/Personnages/{id}")
-    public Personnage modifierUnPersonnage(@PathVariable int id, @RequestBody Personnage personnage) {
+    public Personnage modifierUnPersonnage(@Valid @RequestBody Personnage personnage) {
         return personnagesDao.save(personnage);
     }
 
@@ -89,4 +95,18 @@ public class PersonnagesController {
     public void supprimerUnPersonnage(@PathVariable int id) {
         personnagesDao.deleteById(id);
     }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
+    }
+
 }
